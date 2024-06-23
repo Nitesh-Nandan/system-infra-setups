106e8ffa8421a812d9500b13117d10a6749b8127
Nitesh Nandan <niteshnitp5686@gmail.com>
2024-06-23 18:19:29 +0530

    Initial Setup

--- resources/idgenerator/id-generator-schema.sql
+++ resources/idgenerator/id-generator-schema.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS idgenerator;
 
-DROP TABLE IF EXISTS `id_generator`;
+DROP TABLE IF EXISTS idgenerator.`id_generator`;
 
-CREATE TABLE `id_generator` (
+CREATE TABLE idgenerator.`id_generator` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `stub` char(1) DEFAULT '',
   PRIMARY KEY (`id`)
--- scripts/docker-cli/commands.md
+++ scripts/docker-cli/commands.md
@@ -1,6 +1,17 @@
 # Creating docker-image from docker file 
+
+```commandline
 docker build -t <image_name>:<tag> .
+
 docker build -t cli:1.0 .
 
+docker exec -it docker-cli-cli-1 /bin/bash
+```
+
+
 # mysql-cli
+
+```commandline
+mysql -u root -h mysql-db-1 -p < id-generator-schema.sql
+```
 
